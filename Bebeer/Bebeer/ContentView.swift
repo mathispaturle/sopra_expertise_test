28aa782803a3b4c3b76cb21e1c6e77ab3c55d402
Mathis Paturle <mathipaturle@enti.cat>
2020-07-30 18:20:59 +0200

    Xcode project setup

---
+++ Bebeer/Bebeer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bebeer
+//
+//  Created by Mathis Paturle on 30/07/2020.
+//  Copyright © 2020 Mathis Paturle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
